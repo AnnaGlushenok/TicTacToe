8077072ff055f955859a13bad8e3afd3bf0d0ad8
annas <annasan14052003@gmail.com>
2024-01-03 22:57:05 +0300

    Initial commit

---
+++ app/src/main/java/ticTacToe/tictactoe/MainActivity.kt
@@ -0,0 +1,15 @@
+package ticTacToe.tictactoe
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ticTacToe.tictactoe.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
+    }
+
+}
